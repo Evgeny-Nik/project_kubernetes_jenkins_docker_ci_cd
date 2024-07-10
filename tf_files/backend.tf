2a3c863094cd03146ca58fcffeae0498eb795634
evgenynik <evgenynik92@gmail.com>
2024-07-10 13:14:08 +0300

    edited backend.tf with hardcoded backend+key, and removed vars from Jenkinsfile_init

--- tf_files/backend.tf
+++ tf_files/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "placeholder"
-    key            = "placeholder"
+    bucket         = "tf-kubernetes-jenkins-docker-bucket"
+    key            = "terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-state-lock-dynamo"
     encrypt        = true
